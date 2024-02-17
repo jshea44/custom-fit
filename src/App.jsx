f9761408850d2b97783925ed92ab2ef80c36f789
joshuashea <joshshea44@gmail.com>
2024-02-16 17:37:06 -0800

    added state of workouts on App.jsx as well as added as prop to Home component

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,17 @@ import Home from './Components/Home';
 import Header from './Components/Header';
 
 function App() {
+  const [getWorkouts, setWorkouts] = useState([]);
+
+  const addWorkout = (newWorkout) => {
+    setWorkouts([...getWorkouts, newWorkout]);
+  };
+
   return (
     <>
       <Header />
       <h1>Custom Fit</h1>
-      <Home />
+      <Home workouts={getWorkouts} />
     </>
   );
 }
