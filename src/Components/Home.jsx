41722bd70370809865f26ab98a3fd88f623ba84a
joshuashea <joshshea44@gmail.com>
2024-02-20 16:12:35 -0800

    added MUI button to Home component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,13 @@
+import { Stack, Button } from '@mui/material';
 import Workout from './Workout';
 
 function Home({ workouts }) {
   return (
     <div>
       <h2>Workouts</h2>
+      <Stack spacing={2} direction="row">
+        <Button variant="contained">Create Workout</Button>
+      </Stack>
       <ul>
         {workouts.map((workout, index) => (
           <Workout key={index} workout={workout} />
