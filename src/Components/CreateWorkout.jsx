9360fc8151dd1d7f89e10617cae07943837a9357
joshuashea <joshshea44@gmail.com>
2024-02-23 14:00:35 -0800

    set state of exercises and allowed button press to add another box for user to input another exercise

--- src/Components/CreateWorkout.jsx
+++ src/Components/CreateWorkout.jsx
@@ -11,85 +11,78 @@ import {
   Select,
   MenuItem,
 } from '@mui/material';
+import { useState } from 'react';
 
 function CreateWorkout() {
-  // const [name, setName] = useState('');
-  // const [exercises, setExercises] = useState([]);
+  const [exercises, setExercises] = useState([]);
 
-  // const handleAddExercise = () => {
-  //   // logic to add exercise
-  //   const newExercise = {};
-  //   setExercises([...exercises, newExercise]);
-  // };
-
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   const newWorkout = {
-  //     name: name,
-  //     exercises: exercises,
-  //   };
-  //   onAddWorkout(newWorkout);
-  //   setName('');
-  //   setExercises([]);
-  // };
+  const addExercise = () => {
+    setExercises([
+      ...exercises,
+      {
+        name: '',
+        sets: '',
+        reps: '',
+        weight: '',
+        time: '',
+        distance: '',
+        description: '',
+      },
+    ]);
+  };
 
   return (
     <div>
       <h2>Create Workout</h2>
       <form>
-        <TextField
-          fullWidth
-          label="Workout Name"
-          InputLabelProps={{
-            sx: {
+        <TextField fullWidth label="Workout Name"></TextField>
+        {exercises.map((exercise, index) => (
+          <Box
+            key={index}
+            sx={{
+              border: '2px solid grey',
               display: 'flex',
               justifyContent: 'center',
               alignItems: 'center',
-            },
-          }}
-        ></TextField>
-        <Box
-          sx={{
-            border: '2px solid grey',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <List>
-            <ListItem>
-              <TextField
-                label="Exercise Name"
-                sx={{ width: '25ch' }}
-              ></TextField>
-            </ListItem>
-            <ListItem>
-              <TextField label="Sets" sx={{ width: '25ch' }}></TextField>
-            </ListItem>
-            <ListItem>
-              <TextField label="Reps" sx={{ width: '25ch' }}></TextField>
-            </ListItem>
-            <ListItem>
-              <TextField label="Weight" sx={{ width: '25ch' }}></TextField>
-            </ListItem>
-            <ListItem>
-              <TextField label="Time" sx={{ width: '25ch' }}></TextField>
-            </ListItem>
-            <ListItem>
-              <TextField label="Distance" sx={{ width: '25ch' }}></TextField>
-            </ListItem>
-            <ListItem>
-              <TextField
-                label="Add a description..."
-                multiline
-                rows={4}
-                sx={{ width: '25ch' }}
-              ></TextField>
-            </ListItem>
-          </List>
-        </Box>
+            }}
+          >
+            <List>
+              <ListItem>
+                <TextField
+                  label="Exercise Name"
+                  sx={{ width: '25ch' }}
+                ></TextField>
+              </ListItem>
+              <ListItem>
+                <TextField label="Sets" sx={{ width: '25ch' }}></TextField>
+              </ListItem>
+              <ListItem>
+                <TextField label="Reps" sx={{ width: '25ch' }}></TextField>
+              </ListItem>
+              <ListItem>
+                <TextField label="Weight" sx={{ width: '25ch' }}></TextField>
+              </ListItem>
+              <ListItem>
+                <TextField label="Time" sx={{ width: '25ch' }}></TextField>
+              </ListItem>
+              <ListItem>
+                <TextField label="Distance" sx={{ width: '25ch' }}></TextField>
+              </ListItem>
+              <ListItem>
+                <TextField
+                  label="Add a description..."
+                  multiline
+                  rows={4}
+                  sx={{ width: '25ch' }}
+                ></TextField>
+              </ListItem>
+            </List>
+          </Box>
+        ))}
         <Divider>
-          <Button type="submit">Add Another Exercise</Button>
+          <Button type="button" onClick={addExercise}>
+            Add An Exercise
+          </Button>
         </Divider>
         <Button type="submit">Create</Button>
         <Button type="submit">Cancel</Button>
