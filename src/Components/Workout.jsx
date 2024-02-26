c62c9fe0252c5d355ffe4946617b2c8cd950fc31
joshuashea <joshshea44@gmail.com>
2024-02-26 15:31:14 -0800

    Workout component is now rendering exercises from local sample data

--- src/Components/Workout.jsx
+++ src/Components/Workout.jsx
@@ -1,11 +1,30 @@
+import React from 'react';
+import { Box } from '@mui/material';
 function Workout() {
-  // const sampleData = {
-
-  // };
+  const sampleExercises = [
+    {
+      name: 'pullups',
+      sets: '3',
+      reps: '10',
+    },
+    {
+      name: 'pushups',
+      sets: '3',
+      time: '15 seconds',
+    },
+  ];
 
   return (
     <div>
-      <p>This is the Workout page.</p>
+      {sampleExercises.map((exercise, index) => (
+        <Box key={index} sx={{ border: 'solid red 2px' }}>
+          <p>{exercise.name}</p>
+          <p>{exercise.sets}</p>
+          <p>{exercise.reps}</p>
+          <p>{exercise.time}</p>
+          <p>{exercise.distance}</p>
+        </Box>
+      ))}
     </div>
   );
 }
