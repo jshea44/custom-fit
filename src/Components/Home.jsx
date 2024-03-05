fd3bffd63cac524d34b7a5383c0266a205ca1b46
joshuashea <joshshea44@gmail.com>
2024-03-05 10:01:51 -0800

    connected front and back end allowing workouts to be read from the data base and be shown on the Home component. Also installed react-router-dom

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,14 +1,24 @@
 import { Button, Stack } from '@mui/material';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+const API_SERVER = import.meta.env.VITE_APP_API;
 // import Workout from './Workout';
 
-const sampleWorkouts = [
-  'Full-Body Mondays',
-  'Chest & Back',
-  'Pull, Push, and Legs',
-  'Cardio Xtreme',
-];
-
 function Home({ onCreateWorkoutClick, onWorkoutClick }) {
+  const [workouts, setWorkouts] = useState([]);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      try {
+        const response = await axios.get(`${API_SERVER}/workout`);
+        setWorkouts(response.data);
+      } catch (error) {
+        console.error('Error fetching workouts', error);
+      }
+    };
+    fetchWorkouts();
+  }, []);
+
   return (
     <div>
       <h2>Workouts</h2>
@@ -18,9 +28,9 @@ function Home({ onCreateWorkoutClick, onWorkoutClick }) {
       </Button>
 
       <Stack spacing={2} direction={'column'}>
-        {sampleWorkouts.map((workout, index) => (
-          <Button onClick={onWorkoutClick} key={index}>
-            {workout}
+        {workouts.map((workout, _id) => (
+          <Button onClick={onWorkoutClick} key={_id}>
+            {workout.name}
           </Button>
         ))}
       </Stack>
