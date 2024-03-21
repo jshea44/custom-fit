44438cb6b63363b2b5bdb34ac465968e50a26cd7
joshuashea <joshshea44@gmail.com>
2024-03-21 15:04:46 -0700

    added ternary to show EditWorkout component or Workout component dependent on editMode state

--- src/Components/Workout.jsx
+++ src/Components/Workout.jsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from 'react';
 import { Box, Button, Modal, Typography } from '@mui/material';
 import axios from 'axios';
 const API_SERVER = import.meta.env.VITE_APP_API;
+import EditWorkout from './EditWorkout';
 // import '../App.css';
 
 function Workout({ workoutId, onDeleteButtonClick }) {
   const [exercises, setExercises] = useState([]);
   const [modal, setModal] = useState(false);
+  const [editMode, setEditMode] = useState(false);
 
   useEffect(() => {
     const fetchExercises = async () => {
@@ -30,65 +32,76 @@ function Workout({ workoutId, onDeleteButtonClick }) {
     }
   };
 
+  const handleEditButtonClick = () => {
+    setEditMode(true);
+  };
+
   return (
     <div>
-      <Button
-        type="button"
-        variant="contained"
-        // sx={{ color: 'white', backgroundColor: 'blue' }}
-      >
-        EDIT
-      </Button>
-      <Button
-        onClick={() => setModal(true)}
-        type="button"
-        variant="contained"
-        color="error"
-        // sx={{ color: 'white', backgroundColor: 'red' }}
-      >
-        DELETE
-      </Button>
-
-      <Modal open={modal}>
-        <Box
-          sx={{
-            position: 'absolute',
-            top: '40%',
-            left: '40%',
-            transform: 'translate(-20%, -60%)',
-            width: 400,
-            color: 'black',
-            bgcolor: 'darkgray',
-            border: 'solid green 2px',
-            boxShadow: 24,
-            p: 4,
-          }}
-        >
-          <Typography>Are you sure you want to delete this workout?</Typography>
+      {editMode ? (
+        <EditWorkout />
+      ) : (
+        <>
           <Button
+            onClick={handleEditButtonClick}
+            type="button"
+            variant="contained"
+          >
+            EDIT
+          </Button>
+          <Button
+            onClick={() => setModal(true)}
+            type="button"
             variant="contained"
             color="error"
-            onClick={handleDeleteWorkoutClick}
           >
             DELETE
           </Button>
-          <Button variant="outlined" onClick={() => setModal(false)}>
-            CANCEL
-          </Button>
-        </Box>
-      </Modal>
 
-      {exercises.map((exercise, _id) => (
-        <Box key={_id} sx={{ border: 'solid red 2px', margin: '5px' }}>
-          <p>{exercise.name}</p>
-          <p>{exercise.sets}</p>
-          <p>{exercise.reps}</p>
-          <p>{exercise.weight}</p>
-          <p>{exercise.time}</p>
-          <p>{exercise.distance}</p>
-          <p>{exercise.description}</p>
-        </Box>
-      ))}
+          <Modal open={modal}>
+            <Box
+              sx={{
+                position: 'absolute',
+                top: '40%',
+                left: '40%',
+                transform: 'translate(-20%, -60%)',
+                width: 400,
+                color: 'black',
+                bgcolor: 'darkgray',
+                border: 'solid green 2px',
+                boxShadow: 24,
+                p: 4,
+              }}
+            >
+              <Typography>
+                Are you sure you want to delete this workout?
+              </Typography>
+              <Button
+                variant="contained"
+                color="error"
+                onClick={handleDeleteWorkoutClick}
+              >
+                DELETE
+              </Button>
+              <Button variant="outlined" onClick={() => setModal(false)}>
+                CANCEL
+              </Button>
+            </Box>
+          </Modal>
+
+          {exercises.map((exercise, _id) => (
+            <Box key={_id} sx={{ border: 'solid red 2px', margin: '5px' }}>
+              <p>{exercise.name}</p>
+              <p>{exercise.sets}</p>
+              <p>{exercise.reps}</p>
+              <p>{exercise.weight}</p>
+              <p>{exercise.time}</p>
+              <p>{exercise.distance}</p>
+              <p>{exercise.description}</p>
+            </Box>
+          ))}
+        </>
+      )}
     </div>
   );
 }
