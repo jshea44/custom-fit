4b1f846762e87f23d3d02097e630738302f7d71c
joshuashea <joshshea44@gmail.com>
2024-04-01 17:31:17 -0700

    UserLogin component currently only has text, no functionality. imported UserLogin to App.jsx, state is currently set to false

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from './Components/Header';
 import CreateWorkout from './Components/CreateWorkout';
 import Workout from './Components/Workout';
 import Footer from './Components/Footer';
+import UserLogin from './Components/Login';
 import './App.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
   const [createWorkoutComponent, setCreateWorkoutComponent] = useState(false);
   const [workoutComponent, setWorkoutComponent] = useState(false);
   const [workoutId, setWorkoutId] = useState(null);
+  const [loginPage, setLoginPage] = useState(false);
 
   const handleCreateWorkoutClick = () => {
     setCreateWorkoutComponent(true);
@@ -33,23 +35,29 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Header onHomeButtonClick={handleHomeButtonClick} />
-      {homeComponent && (
-        <Home
-          onCreateWorkoutClick={handleCreateWorkoutClick}
-          onWorkoutClick={handleWorkoutClick}
-        />
+      {loginPage ? (
+        <UserLogin />
+      ) : (
+        <>
+          <Header onHomeButtonClick={handleHomeButtonClick} />
+          {homeComponent && (
+            <Home
+              onCreateWorkoutClick={handleCreateWorkoutClick}
+              onWorkoutClick={handleWorkoutClick}
+            />
+          )}
+          {createWorkoutComponent && (
+            <CreateWorkout onCancelButtonClick={handleHomeButtonClick} />
+          )}
+          {workoutComponent && (
+            <Workout
+              onDeleteButtonClick={handleHomeButtonClick}
+              workoutId={workoutId}
+            />
+          )}
+          <Footer />
+        </>
       )}
-      {createWorkoutComponent && (
-        <CreateWorkout onCancelButtonClick={handleHomeButtonClick} />
-      )}
-      {workoutComponent && (
-        <Workout
-          onDeleteButtonClick={handleHomeButtonClick}
-          workoutId={workoutId}
-        />
-      )}
-      <Footer />
     </BrowserRouter>
   );
 }
---
+++ src/Components/UserLogin.jsx
@@ -0,0 +1,9 @@
+function UserLogin() {
+  return (
+    <div>
+      <h2>This is the Login Screen</h2>
+    </div>
+  );
+}
+
+export default UserLogin;
