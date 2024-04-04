bb80251c012ec99e16c0ce04a0d304b887dadb24
joshuashea <joshshea44@gmail.com>
2024-04-03 17:39:47 -0700

    CreateUser component mounts when create account button is clicked

--- src/Components/UserLogin.jsx
+++ src/Components/UserLogin.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import CreateUser from './CreateUser';
 
 function UserLogin({ loginClick }) {
-  const [createUserPage, setCreateUserPage] = useState(true);
+  const [createUserPage, setCreateUserPage] = useState(false);
   const handleLoginSubmit = () => {
     // add logic for loging in (axios)
     loginClick();
@@ -20,14 +20,19 @@ function UserLogin({ loginClick }) {
       }}
     >
       {createUserPage ? (
-        <CreateUser />
+        <CreateUser
+        // cancelCreateClick={handleCancelCreate}
+        // createClick={handleCreateUser}
+        />
       ) : (
         <form onSubmit={handleLoginSubmit}>
           <h2>Please login or create an account</h2>
           <TextField label="username"></TextField>
           <TextField label="password"></TextField>
           <Button type="submit">login</Button>
-          <Button type="button">Create An Account</Button>
+          <Button type="button" onClick={() => setCreateUserPage(true)}>
+            Create An Account
+          </Button>
         </form>
       )}
     </Box>
