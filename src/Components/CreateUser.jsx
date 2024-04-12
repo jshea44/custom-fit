67baa06f30b2c99cf54c2309ed3717478e6d1714
joshuashea <joshshea44@gmail.com>
2024-04-12 10:18:47 -0700

    can now create a new account from the CreateUser component

--- src/Components/CreateUser.jsx
+++ src/Components/CreateUser.jsx
@@ -1,23 +1,51 @@
 import { Box, TextField, Button } from '@mui/material';
+import { useState } from 'react';
 import axios from 'axios';
 const API_SERVER = import.meta.env.VITE_APP_API;
 
 function CreateUser({ cancelCreateClick }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   const handleCreateButtonClick = async () => {
     try {
       // logic to create account (axios call)
+      const response = await axios.post(`${API_SERVER}/signup`, {
+        username: username,
+        password: password,
+      });
     } catch (error) {
       console.error('Error creating account', error);
     }
   };
 
+  const handleInputChange = (field, value) => {
+    if (field === 'username') {
+      setUsername(value);
+    } else if (field === 'password') {
+      setPassword(value);
+    }
+  };
+
   return (
     <Box>
-      <form>
+      <form onSubmit={handleCreateButtonClick}>
         <h2>Create Account</h2>
-        <TextField label="Enter a username"></TextField>
-        <TextField label="Enter a password"></TextField>
-        <Button>Create</Button>
+        <TextField
+          label="Enter a username"
+          value={username}
+          onChange={(e) => {
+            handleInputChange('username', e.target.value);
+          }}
+        ></TextField>
+        <TextField
+          label="Enter a password"
+          value={password}
+          onChange={(e) => {
+            handleInputChange('password', e.target.value);
+          }}
+        ></TextField>
+        <Button type="submit">Create</Button>
         <Button onClick={cancelCreateClick}>Cancel</Button>
       </form>
     </Box>
